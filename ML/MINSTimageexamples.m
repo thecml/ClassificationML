13806fce6b359b7904e5456cb0ff9da2628a449a
Christian Lillelund <chr1000@gmail.com>
2018-11-28 22:05:30 +0100

    Fixed bug in NSC

--- ML/MINSTimageexamples.m
+++ ML/MINSTimageexamples.m
@@ -3,16 +3,16 @@ clear all
 [trainData,testData,trainLbls,testLbls] = load_MNIST();
 
 
-img1 = reshape(trainData(:,22),[28,28]);%
-img2 = reshape(trainData(:,71),[28,28]);%
-img3 = reshape(trainData(:,110),[28,28]);%
-img4 = reshape(trainData(:,99),[28,28]);%
-img5 = reshape(trainData(:,21),[28,28]);%
-img6 = reshape(trainData(:,1),[28,28]);%
-img7 = reshape(trainData(:,19),[28,28]);%
-img8 = reshape(trainData(:,10000),[28,28]);
-img9 = reshape(trainData(:,18),[28,28]);%
-img10 = reshape(trainData(:,9000),[28,28]);%
+img1 = reshape(trainData(:,54),[28,28]);%
+img2 = reshape(trainData(:,32),[28,28]);%
+img3 = reshape(trainData(:,120),[28,28]);%
+img4 = reshape(trainData(:,654),[28,28]);%
+img5 = reshape(trainData(:,2),[28,28]);%
+img6 = reshape(trainData(:,541),[28,28]);%
+img7 = reshape(trainData(:,631),[28,28]);%
+img8 = reshape(trainData(:,1000),[28,28]);
+img9 = reshape(trainData(:,44),[28,28]);%
+img10 = reshape(trainData(:,8000),[28,28]);%
 
 image = [ img1 ones(28,2) img2 ones(28,2) img3 ones(28,2) img4 ones(28,2) img5 ; 
           ones(2,28*5+2*4);
@@ -20,4 +20,4 @@ image = [ img1 ones(28,2) img2 ones(28,2) img3 ones(28,2) img4 ones(28,2) img5 ;
 
 imshow(image);
 
-imwrite(image,'../Report/dataset/MNIST.png')
+%imwrite(image,'../Report/dataset/MNIST.png')
--- ML/ORLimageexamples.m
+++ ML/ORLimageexamples.m
@@ -1,23 +1,23 @@
 clear all
 
-load('../ORL/orl_data.mat');
-load('../ORL/orl_lbls.mat');
+load('data/orl_data.mat');
+load('data/orl_lbls.mat');
 
 
-img1 = reshape(data(:,1),[40,30]);
-img2 = reshape(data(:,22),[40,30]);
-img3 = reshape(data(:,33),[40,30]);
-img4 = reshape(data(:,44),[40,30]);
-img5 = reshape(data(:,55),[40,30]);
-img6 = reshape(data(:,66),[40,30]);
-img7 = reshape(data(:,77),[40,30]);
-img8 = reshape(data(:,88),[40,30]);
-img9 = reshape(data(:,99),[40,30]);
-img10 = reshape(data(:,110),[40,30]);
+img1 = reshape(data(:,121),[40,30]);
+img2 = reshape(data(:,132),[40,30]);
+img3 = reshape(data(:,143),[40,30]);
+img4 = reshape(data(:,154),[40,30]);
+img5 = reshape(data(:,165),[40,30]);
+img6 = reshape(data(:,176),[40,30]);
+img7 = reshape(data(:,187),[40,30]);
+img8 = reshape(data(:,198),[40,30]);
+img9 = reshape(data(:,209),[40,30]);
+img10 = reshape(data(:,220),[40,30]);
 
 image = [ img1 ones(40,2) img2 ones(40,2) img3 ones(40,2) img4 ones(40,2) img5 ; 
           ones(2,30*5+2*4);
           img6 ones(40,2) img7 ones(40,2) img8 ones(40,2) img9  ones(40,2) img10]
 
 imshow(image);
-imwrite(image,'../Report/dataset/ORL.png')
+%imwrite(image,'../Report/dataset/ORL.png')
